fc8d23e14021fb06d0d7dcd34470a1113f97bab5
interactivedevelop <sergi@systtekinteractive.com>
2021-10-15 08:04:05 +0200

    Groovy Files Update

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,10 @@
+/* groovylint-disable-next-line CompileStatic */
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/interactivedevelop/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('interactivedevelop')
+            node / gitConfigEmail('sergi@systtekinteracyive.com')
         }
     }
     triggers {
@@ -14,7 +15,8 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('interactivedevelop/nodejsapp')
+            /* groovylint-disable-next-line GStringExpressionWithinString */
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
@@ -22,23 +24,23 @@ job('Aplicacion Node.js Docker DSL') {
             skipDecorate()
         }
     }
-    publishers {
-    slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
-        }
-    }
+    // publishers {
+    // slackNotifier {
+    //         notifyAborted(true)
+    //         notifyEveryFailure(true)
+    //         notifyNotBuilt(false)
+    //         notifyUnstable(false)
+    //         notifyBackToNormal(true)
+    //         notifySuccess(true)
+    //         notifyRepeatedFailure(false)
+    //         startNotification(false)
+    //         includeTestSummary(false)
+    //         includeCustomMessage(false)
+    //         customMessage(null)
+    //         sendAs(null)
+    //         commitInfoChoice('NONE')
+    //         teamDomain(null)
+    //         authToken(null)
+    //     }
+    // }
 }
