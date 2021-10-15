48537808e2361c15d65b333265d8b546caca6e79
interactivedevelop <sergi@systtekinteractive.com>
2021-10-15 08:01:46 +0200

    Groovy File Update

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,10 @@
+/* groovylint-disable-next-line CompileStatic */
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/interactivedevelop/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('interactivedevelop')
+            node / gitConfigEmail('sergi@systtekinteracyive.com')
         }
     }
     triggers {
@@ -13,25 +14,25 @@ job('Aplicacion Node.js DSL') {
         nodejs('nodejs')
     }
     steps {
-        shell("npm install")
+        shell('npm install')
     }
-    publishers {
-	slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
-        }
-    }
+    // publishers {
+    // slackNotifier {
+    //         notifyAborted(true)
+    //         notifyEveryFailure(true)
+    //         notifyNotBuilt(false)
+    //         notifyUnstable(false)
+    //         notifyBackToNormal(true)
+    //         notifySuccess(true)
+    //         notifyRepeatedFailure(false)
+    //         startNotification(false)
+    //         includeTestSummary(false)
+    //         includeCustomMessage(false)
+    //         customMessage(null)
+    //         sendAs(null)
+    //         commitInfoChoice('NONE')
+    //         teamDomain(null)
+    //         authToken(null)
+    //     }
+    // }
 }
